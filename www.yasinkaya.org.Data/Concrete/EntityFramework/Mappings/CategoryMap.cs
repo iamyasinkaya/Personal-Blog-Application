8b650befcea65cb030a9f126c7b3d72217f59ecd
Yasin Kaya <yasin.kaya@innthebox.com>
2021-06-05 14:56:03 +0300

    The map of the Category class has been created!

---
+++ www.yasinkaya.org.Data/Concrete/EntityFramework/Mappings/CategoryMap.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using www.yasinkaya.org.Entities.Concrete;
+
+namespace www.yasinkaya.org.Data.Concrete.EntityFramework.Mappings
+{
+    public class CategoryMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id).ValueGeneratedOnAdd();
+            builder.Property(c => c.Name).IsRequired().HasMaxLength(50);
+            builder.Property(c => c.Description).HasMaxLength(250);
+            builder.Property(c => c.CreatedByName).IsRequired().HasMaxLength(50);
+            builder.Property(c => c.ModifiedByName).IsRequired().HasMaxLength(50);
+            builder.Property(c => c.CreatedDate).IsRequired();
+            builder.Property(c => c.ModifiedDate).IsRequired();
+            builder.Property(c => c.IsDeleted).IsRequired();
+            builder.Property(c => c.IsActive).IsRequired();
+            builder.Property(c => c.Note).HasMaxLength(500);
+            builder.ToTable("Categories");
+        }
+    }
+}
