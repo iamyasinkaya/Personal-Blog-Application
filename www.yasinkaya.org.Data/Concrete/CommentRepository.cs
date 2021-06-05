0954f40688ee89df1b7cdc6931a8630326d935ba
Yasin Kaya <yasin.kaya@innthebox.com>
2021-06-05 14:14:48 +0300

    Our repository classes deriving from EfEntityRepositoryBase have been created!

---
+++ www.yasinkaya.org.Data/Concrete/CommentRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using www.yasinkaya.org.Data.Abstract;
+using www.yasinkaya.org.Entities.Concrete;
+using www.yasinkaya.org.Shared.Data.Concrete.EntityFramework;
+
+namespace www.yasinkaya.org.Data.Concrete
+{
+    public class CommentRepository : EfEntityRepositoryBase<Comment>, ICommentRepository
+    {
+        public CommentRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
