303d764c39168ce4fda7b46fc1a438af176f6c07
Yasin Kaya <yasin.kaya@innthebox.com>
2021-06-05 14:48:47 +0300

    The map of the Article class has been created!

---
+++ www.yasinkaya.org.Data/Concrete/EntityFramework/Mappings/ArticleMap.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using www.yasinkaya.org.Entities.Concrete;
+
+namespace www.yasinkaya.org.Data.Concrete.EntityFramework.Mappings
+{
+    public class ArticleMap : IEntityTypeConfiguration<Article>
+    {
+        public void Configure(EntityTypeBuilder<Article> builder)
+        {
+            builder.HasKey(a => a.Id); //Primary Key
+            builder.Property(a => a.Id).ValueGeneratedOnAdd(); // 1'er 1'er artmasını istedim.
+            builder.Property(a => a.Title).HasMaxLength(150); // 150 karakter olmasını istedim.
+            builder.Property(a => a.Title).IsRequired(true); // Boş geçilemez.
+            builder.Property(a => a.Content).IsRequired(true);
+            builder.Property(a => a.Content).HasColumnType("NVARCHAR(MAX)"); // Maksimum değerde alabilmesini istedim.
+            builder.Property(a => a.Date).IsRequired();
+            builder.Property(a => a.SeoAuthor).HasMaxLength(50).IsRequired();
+            builder.Property(a => a.SeoDesc).HasMaxLength(150).IsRequired();
+            builder.Property(a => a.SeoTags).IsRequired().HasMaxLength(70);
+            builder.Property(a => a.ViewsCount).IsRequired();
+            builder.Property(a => a.CommentCount).IsRequired();
+            builder.Property(a => a.Thumbnail).IsRequired().HasMaxLength(250);
+            builder.Property(a => a.CreatedByName).IsRequired().HasMaxLength(50);
+            builder.Property(a => a.ModifiedByName).IsRequired().HasMaxLength(50);
+            builder.Property(a => a.CreatedDate).IsRequired();
+            builder.Property(a => a.ModifiedDate).IsRequired();
+            builder.Property(a => a.IsDeleted).IsRequired();
+            builder.Property(a => a.IsActive).IsRequired();
+            builder.Property(a => a.Note).HasMaxLength(500);
+            builder.HasOne<Category>(a => a.Category).WithMany(c => c.Articles).HasForeignKey(a => a.CategoryId); // 1-N ilişkiyi kurmuş oldum.
+            builder.HasOne<User>(a => a.User).WithMany(u => u.Articles).HasForeignKey(a => a.UserId);
+            builder.ToTable("Articles");
+        }
+    }
+}
