1fef55df32a452efa44b3e37c7b30b8f3e0374dc
Yasin Kaya <yasin.kaya@innthebox.com>
2021-06-06 12:37:09 +0300

    Category Controller, Index Action and Index View has been created

---
+++ www.yasinkaya.org.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using www.yasinkaya.org.Services.Abstract;
+using www.yasinkaya.org.Shared.Utilities.Result.ComplexTypes;
+
+namespace www.yasinkaya.org.Mvc.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _categoryService.GetAllAsync();
+            if (result.ResultStatus == ResultStatus.Success)
+            {
+                return View(result.Data);
+            }
+            return View();
+        }
+    }
+}
