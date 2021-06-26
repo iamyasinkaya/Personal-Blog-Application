a9fef027774cdc22510b9a0d8861296710ab6406
Yasin Kaya <yasin.kaya@innthebox.com>
2021-06-26 11:46:23 +0300

    Let's Improve Our ViewComponents, Add Article Page and Button

--- www.yasinkaya.org.Mvc/Startup.cs
+++ www.yasinkaya.org.Mvc/Startup.cs
@@ -32,7 +32,10 @@ namespace www.yasinkaya.org.Mvc
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews().AddRazorRuntimeCompilation().AddJsonOptions(opt =>
+            services.AddControllersWithViews(options=>
+            {
+                options.ModelBindingMessageProvider.SetValueMustNotBeNullAccessor(value => "Bu alan boþ geçilmemelidir.");
+            }).AddRazorRuntimeCompilation().AddJsonOptions(opt =>
             {
                 opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
                 opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
