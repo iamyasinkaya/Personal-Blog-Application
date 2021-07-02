ea5a5dc222851ed441961117ac87c8303a36b8c3
Yasin Kaya <yasin.kaya@innthebox.com>
2021-07-02 15:14:21 +0300

    Comment Operations

---
+++ www.yasinkaya.org.Mvc/Models/CommentAddAjaxViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using www.yasinkaya.org.Entities.Dtos;
+
+namespace www.yasinkaya.org.Mvc.Models
+{
+    public class CommentAddAjaxViewModel
+    {
+        public CommentAddDto CommentAddDto { get; set; }
+        public string CommentAddPartial { get; set; }
+        public CommentDto CommentDto { get; set; }
+    }
+}
