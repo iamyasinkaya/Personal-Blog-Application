e9cd48d1197421499d9420c99abc19e31482ee5a
Yasin Kaya <yasin.kaya@innthebox.com>
2021-07-02 15:58:04 +0300

    GetAllByUserIdOnFilter() Operasyonu oluşturuldu

--- www.yasinkaya.org.Services/Abstract/IArticleService.cs
+++ www.yasinkaya.org.Services/Abstract/IArticleService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using www.yasinkaya.org.Entities.ComplexTypes;
 using www.yasinkaya.org.Entities.Concrete;
 using www.yasinkaya.org.Entities.Dtos;
 using www.yasinkaya.org.Shared.Utilities.Result.Abstract;
@@ -42,6 +43,8 @@ namespace www.yasinkaya.org.Services.Abstract
         Task<IDataResult<ArticleListDto>> GetAllByViewCountAsync(bool isAscending, int? takeSize);
 
         Task<IDataResult<ArticleListDto>> GetAllByPagingAsync(int? categoryId, int currentPage = 1, int pageSize = 5, bool isAscending = false);
+
+        Task<IDataResult<ArticleListDto>> GetAllByUserIdOnFilterAsnyc(int userId, FilterBy filterBy, OrderBy orderBy, bool isAscending, int takeSize, int categoryId, DateTime startAt, DateTime endAt, int minViewCount, int maxViewCount, int minCommentCount, int maxCommentCount);
 
         Task<IDataResult<ArticleListDto>> SearchAsync(string keyword, int currentPage = 1, int pageSize = 5, bool isAscending = false);
 
