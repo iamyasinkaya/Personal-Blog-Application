7ba6be363170bbcb49912453f4204894d3da50dd
Yasin Kaya <yasin.kaya@innthebox.com>
2021-07-04 13:19:08 +0300

    EfEntityRepositoryBase için GetAsyncV2 & GetAllAsyncV2 Metodları Oluşturuldu.

--- www.yasinkaya.org.Shared/Data/Concrete/EntityFramework/EfEntityRepositoryBase.cs
+++ www.yasinkaya.org.Shared/Data/Concrete/EntityFramework/EfEntityRepositoryBase.cs
@@ -18,28 +18,46 @@ namespace www.yasinkaya.org.Shared.Data.Concrete.EntityFramework
         public EfEntityRepositoryBase(DbContext context)
         {
             _context = context;
-           // _context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            // _context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+        public async Task<T> GetAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            query = query.Where(predicate);
+
+            if (includeProperties.Any())
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            return await query.AsNoTracking().SingleOrDefaultAsync();
         }
 
-        public async Task<T> AddAsync(T entity)
+        public async Task<T> GetAsyncV2(IList<Expression<Func<T, bool>>> predicates, IList<Expression<Func<T, object>>> includeProperties)
         {
-            await _context.Set<T>().AddAsync(entity);
-            return entity;
-        }
+            IQueryable<T> query = _context.Set<T>();
 
-        public async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate)
-        {
-            return await _context.Set<T>().AnyAsync(predicate);
-        }
+            if (predicates != null && predicates.Any())
+            {
+                foreach (var predicate in predicates)
+                {
+                    query = query.Where(predicate);
+                }
+            }
 
-        public async Task<int> CountAsync(Expression<Func<T, bool>> predicate = null)
-        {
-            return await (predicate == null ? _context.Set<T>().CountAsync() : _context.Set<T>().CountAsync(predicate));
-        }
+            if (includeProperties != null && includeProperties.Any())
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
 
-        public async Task DeleteAsync(T entity)
-        {
-            await Task.Run(() => { _context.Set<T>().Remove(entity); });
+            return await query.AsNoTracking().SingleOrDefaultAsync();
         }
 
         public async Task<IList<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null, params Expression<Func<T, object>>[] includeProperties)
@@ -62,13 +80,20 @@ namespace www.yasinkaya.org.Shared.Data.Concrete.EntityFramework
             return await query.AsNoTracking().ToListAsync();
         }
 
-        public async Task<T> GetAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties)
+        public async Task<IList<T>> GetAllAsyncV2(IList<Expression<Func<T, bool>>> predicates, IList<Expression<Func<T, object>>> includeProperties)
         {
+
             IQueryable<T> query = _context.Set<T>();
 
-            query = query.Where(predicate);
+            if (predicates != null && predicates.Any())
+            {
+                foreach (var predicate in predicates)
+                {
+                    query = query.Where(predicate);
+                }
+            }
 
-            if (includeProperties.Any())
+            if (includeProperties != null && includeProperties.Any())
             {
                 foreach (var includeProperty in includeProperties)
                 {
@@ -76,7 +101,34 @@ namespace www.yasinkaya.org.Shared.Data.Concrete.EntityFramework
                 }
             }
 
-            return await query.AsNoTracking().SingleOrDefaultAsync();
+            return await query.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            return entity;
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            await Task.Run(() => { _context.Set<T>().Remove(entity); });
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            await Task.Run(() => { _context.Set<T>().Update(entity); });
+            return entity;
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().AnyAsync(predicate);
+        }
+
+        public async Task<int> CountAsync(Expression<Func<T, bool>> predicate = null)
+        {
+            return await (predicate == null ? _context.Set<T>().CountAsync() : _context.Set<T>().CountAsync(predicate));
         }
 
         public async Task<IList<T>> SearchAsync(IList<Expression<Func<T, bool>>> predicates, params Expression<Func<T, object>>[] includeProperties)
@@ -106,10 +158,6 @@ namespace www.yasinkaya.org.Shared.Data.Concrete.EntityFramework
             return await query.ToListAsync();
         }
 
-        public async Task<T> UpdateAsync(T entity)
-        {
-            await Task.Run(() => { _context.Set<T>().Update(entity); });
-            return entity;
-        }
+       
     }
 }
